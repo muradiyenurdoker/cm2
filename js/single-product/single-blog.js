6af074c9a8925fecc10ba2e5f5cb56ab5650a6aa
muradiye nur <nurdoker45@gmail.com>
2025-03-03 22:59:43 +0300

    commit

---
+++ js/single-product/single-blog.js
@@ -0,0 +1,14 @@
+// Fancybox Configuration
+$('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+      "slideShow",
+      "thumbs",
+      "zoom",
+      "fullScreen",
+      "share",
+      "close"
+    ],
+    loop: false,
+    protect: true
+  });
+  
